c316aa393e549d66db802ceb678a893903f79e31
lphillipe <luisphillipe200@gmail.com>
2025-09-30 07:01:32 -0300

    add

---
+++ reviewweek2.1.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <ctype>
+
+int main(void)
+{
+    int freq[26] = {0};
+    string texto = get_string("Digite um texto: ");
+
+    for (int i = 0; texto[i] != '\0'; i++)
+    {
+
+    }
+
+    for (int i = 0; i < 26; i++)
+
+    {
+        
+    }
+}
