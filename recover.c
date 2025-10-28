f00a580e57fb5b601d16c15790fb537c37f62252
lphillipe <luisphillipe200@gmail.com>
2025-10-27 22:31:05 -0300

    add problemset4

---
+++ recover.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    FILE *file = fopen("card.raw", "rb");
+    if (file == NULL)
+    {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    unsigned char buffer[512];
+    size_t bytes = fread(buffer, 1, 512, file);
+
+    printf("Bytes lidos: %zu\n", bytes);
+
+    fclose(file);
+
+    int count = 0;
+
+    while (fread(buffer, 1, 512, file) == 512)
+    {
+        count++;
+    }
+
+    printf("Blocos lidos: %d\n", count);
+}
