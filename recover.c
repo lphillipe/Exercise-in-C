7cfa670c24cf7416a0b9452c380faf9860be73d9
lphillipe <luisphillipe200@gmail.com>
2025-10-30 21:24:55 -0300

    add

--- recover.c
+++ recover.c
@@ -1,30 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(void)
 {
-    FILE *file = fopen("card.raw", "rb");
-    if (file == NULL)
+    FILE *infile = fopen("card.raw", "rb");
+    if (infile == NULL)
     {
         printf("Erro ao abrir o arquivo.\n");
         return 1;
     }
 
     unsigned char buffer[512];
-    size_t bytes = fread(buffer, 1, 512, file);
 
-    printf("Bytes lidos: %zu\n", bytes);
+    FILE *img = NULL;
+    int filecount = 0;
+    bool found_jpeg = false;
 
+    // Loop: Ler 512 bytes até o fim
+    while(1)
+    {
+        size_t bytes = fread(buffer, 1, 512, infile);
+
+        if (bytes == 0)
+        {
+            break; // Fim do arquivo
+        }
     
 
-    int count = 0;
+        // Verificar se o buffer contém início de JPEG
+        bool header = buffer[0] == 0xff &&
+                    buffer[1] == 0xd8 &&
+                    buffer[2] == 0xff &&
+                    (buffer[3] & 0xf0) == 0xe0;
 
-    while (fread(buffer, 1, 512, file) == 512)
-    {
-        count++;
+        if (header)
+        {
+            //  Se já havia JPEG sendo escrito, fechar arquivo anterior
+
+            // Criar nome do novo arquivo
+            // abrir novo arquivo
+            // marcar que começamos jpeg
+            // escrever primeiro bloco
+        }
+
+        else
+        {
+            // Se já começamos um JPEG, continuar escrevendo blocos nele
+        }
+
+        if (bytes < 512)
+        {
+        break;
+        }
     }
+    
+    fclose(infile);
+                  
 
-    printf("Blocos lidos: %d\n", count);
-
-    fclose(file);
 }
